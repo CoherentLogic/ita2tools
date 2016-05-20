2d11a1334eb6f1ecd2fb400481e1c89950efc757
John P. Willis <jpw@coherent-logic.com>
2016-05-20 15:42:26 -0600

    Initial Commit

---
+++ src/libita2/libita2.c
@@ -0,0 +1,333 @@
+/*
+ * libita2.c
+ * 
+ *  ITA2 library
+ *   Converts ASCII to ITA2 and vice-versa
+ *
+ *  Copyright (C) 2016 Coherent Logic Development LLC
+ *
+ */
+
+#include <string.h>
+#include <stdint.h>
+
+#define SHIFT_FIGS 0x1B
+#define SHIFT_LTRS 0x1F
+
+#define ITA2_BOTH 0xFC
+#define ITA2_FIGS 0xFD
+#define ITA2_LTRS 0xFE
+#define ITA2_NONE 0xFF
+
+int ita2asc(char *, char*);
+int asc2ita(char *, char*);
+
+/* BOTH SETS = 0xFC; FIGS = 0xFD; LTRS = 0xFE; NO EQUIVALENT = 0xFF */
+static const uint8_t ita2_ascii_ltrs[32] = {
+  0x00, // NUL
+  0x45, // E
+  0x0A, // LF
+  0x41, // A
+  0x20, // SPACE
+  0x53, // S
+  0x49, // I
+  0x55, // U
+  0x0D, // CR
+  0x44, // D
+  0x52, // R
+  0x4A, // J
+  0x4E, // N
+  0x46, // F
+  0x43, // C
+  0x4B, // K
+  0x54, // T
+  0x5A, // Z
+  0x4C, // L
+  0x57, // W
+  0x48, // H
+  0x59, // Y
+  0x50, // P
+  0x51, // Q
+  0x4F, // O
+  0x42, // B
+  0x47, // G
+  0xFD, // FIGS
+  0x4D, // M
+  0x58, // X
+  0x56, // V
+  0xFE  // LTRS
+};
+
+static const uint8_t ita2_ascii_figs[32] = {
+  0x00, // NUL
+  0x33, // 3
+  0x0A, // LF
+  0x2D, // -
+  0x20, // SPACE
+  0x27, // '
+  0x38, // 8
+  0x37, // 7
+  0x0D, // CR
+  0x05, // ENQ
+  0x34, // 4
+  0x07, // BEL
+  0x2C, // ,
+  0x21, // !
+  0x3A, // :
+  0x28, // (
+  0x35, // 5
+  0x2B, // +
+  0x29, // )
+  0x32, // 2
+  0x24, // $
+  0x36, // 6
+  0x30, // 0
+  0x31, // 1
+  0x39, // 9
+  0x3F, // ?
+  0x26, // &
+  0xFD, // FIGS
+  0x2E, // .
+  0x2F, // /
+  0x3B, // ;
+  0xFE  // LTRS
+};
+
+/*
+ * ASCII -> ITA2 lookup table 
+ *
+ * SHIFT state is in MSB; encoding in LSB 
+ *
+ * MSB states:
+ *  FC = LSB represents the same character in both LTRS and FIGS
+ *  FD = LSB represents a character in FIGS
+ *  FE = LSB represents a character in LTRS
+ *  FF = LSB represents a character that should be ignored
+ *           (no equivalent representation in ITA2 encoding)
+ * 
+ *  Note that all lowercase characters (ASCII 0x61-0x7A) will
+ *  be converted to uppercase (ASCII 0x41-0x5A).
+ *  
+ *  Double quotes (") will be converted into single quotes (').
+ */
+static const uint16_t ascii_ita2[128] = {
+  0xFC00, // NUL
+  0xFFFF, // SOH
+  0xFFFF, // STX
+  0xFFFF, // ETX
+  0xFFFF, // EOT
+  0xFD09, // ENQ
+  0xFFFF, // ACK
+  0xFD0B, // BEL
+  0xFFFF, // BS
+  0xFFFF, // TAB
+  0xFC02, // LF
+  0xFFFF, // VT
+  0xFFFF, // FF
+  0xFC08, // CR
+  0xFFFF, // SO
+  0xFFFF, // SI
+  0xFFFF, // DLE
+  0xFFFF, // DC1, XON
+  0xFFFF, // DC2
+  0xFFFF, // DC3, XOFF
+  0xFFFF, // DC4
+  0xFFFF, // NAK
+  0xFFFF, // SYN
+  0xFFFF, // ETB
+  0xFFFF, // CAN
+  0xFFFF, // EM
+  0xFFFF, // SUB
+  0xFFFF, // ESC
+  0xFFFF, // FS
+  0xFFFF, // GS
+  0xFFFF, // RS
+  0xFFFF, // US
+  0xFC04, // SPACE
+  0xFD0D, // !
+  0xFD27, // " (substitute with single quote here)
+  0xFFFF, // #
+  0xFD14, // $
+  0xFFFF, // %
+  0xFD26, // &
+  0xFD27, // '
+  0xFD0F, // (
+  0xFD12, // )
+  0xFFFF, // *
+  0xFD11, // +
+  0xFD0C, // ,
+  0xFD03, // -
+  0xFD1C, // .
+  0xFD1D, // /
+  0xFD16, // 0
+  0xFD17, // 1
+  0xFD13, // 2
+  0xFD01, // 3
+  0xFD0A, // 4
+  0xFD10, // 5
+  0xFD15, // 6
+  0xFD07, // 7
+  0xFD06, // 8
+  0xFD18, // 9
+  0xFD0E, // :
+  0xFD1E, // ;
+  0xFFFF, // <
+  0xFFFF, // =
+  0xFFFF, // >
+  0xFD19, // ?
+  0xFFFF, // @
+  0xFE03, // A
+  0xFE19, // B
+  0xFE0E, // C
+  0xFE09, // D
+  0xFE01, // E
+  0xFE0D, // F
+  0xFE1A, // G
+  0xFE14, // H
+  0xFE06, // I
+  0xFE0B, // J
+  0xFE0F, // K
+  0xFE12, // L
+  0xFE1C, // M
+  0xFE0C, // N
+  0xFE18, // O
+  0xFE16, // P
+  0xFE17, // Q
+  0xFE0A, // R
+  0xFE05, // S
+  0xFE10, // T
+  0xFE07, // U
+  0xFE1E, // V
+  0xFE13, // W
+  0xFE1D, // X
+  0xFE15, // Y
+  0xFE11, // Z
+  0xFFFF, // [
+  0xFFFF, /* \ */
+  0xFFFF, // ]
+  0xFFFF, // ^
+  0xFFFF, // _
+  0xFFFF, // `
+  0xFE03, // A
+  0xFE19, // B
+  0xFE0E, // C
+  0xFE09, // D
+  0xFE01, // E
+  0xFE0D, // F
+  0xFE1A, // G
+  0xFE14, // H
+  0xFE06, // I
+  0xFE0B, // J
+  0xFE0F, // K
+  0xFE12, // L
+  0xFE1C, // M
+  0xFE0C, // N
+  0xFE18, // O
+  0xFE16, // P
+  0xFE17, // Q
+  0xFE0A, // R
+  0xFE05, // S
+  0xFE10, // T
+  0xFE07, // U
+  0xFE1E, // V
+  0xFE13, // W
+  0xFE1D, // X
+  0xFE15, // Y
+  0xFE11, // Z
+  0xFFFF, // {
+  0xFFFF, // |
+  0xFFFF, // }
+  0xFFFF, // ~
+  0xFFFF  // DEL
+};
+
+int ita2asc(char *input, char *output) 
+{
+  int input_len;
+  char c;
+
+  int pos = 0;
+
+  uint8_t shift_state = SHIFT_LTRS;
+  uint8_t conv;
+
+  input_len = strlen(input);
+
+  for(int i = 0; i < input_len; i++) {
+    c = input[i] & 0x1F; // mask off top 3 most significant bits (we want pure ITA2)
+
+    switch(c) {
+    case SHIFT_FIGS:
+      shift_state = SHIFT_FIGS;
+      break;
+    case SHIFT_LTRS:
+      shift_state = SHIFT_LTRS;
+      break;
+    default:
+      if(shift_state == SHIFT_FIGS) {
+	conv = ita2_ascii_figs[c];
+      }
+      else {
+	conv = ita2_ascii_ltrs[c];
+      }
+
+      output[pos++] = (char) conv;
+    }
+  }
+
+  return(input_len);
+}
+
+int asc2ita(char *input, char *output)
+{
+  int input_len;
+  char c;
+
+  int pos = 2;
+
+  uint16_t conv;
+  uint8_t lsb;
+  uint8_t msb;
+
+  uint8_t shift_state = SHIFT_LTRS;
+
+  // it is customary for an ITA2 output to begin with two LTRS bytes
+  output[0] = SHIFT_LTRS;
+  output[1] = SHIFT_LTRS;
+
+  input_len = strlen(input);
+
+  for(int i = 0; i < input_len; i++) {
+    
+    c = input[i] & 0x7F; // mask off most significant bit (we want pure ASCII)
+   
+    conv = ascii_ita2[c];
+
+    lsb = (uint8_t) (conv & 0x00FF);
+    msb = (uint8_t) ((conv >> 8) & 0xFF00);
+
+    switch(msb) {
+
+    case ITA2_BOTH: // no change in shift state for this char
+      break;
+
+    case ITA2_FIGS: // following char = FIGS
+    case ITA2_LTRS: // following char = LTRS
+      
+      if(msb != shift_state) {
+	output[pos] = (char) msb;
+	shift_state = msb;
+	pos++;
+      }
+
+      output[pos++] = (char) lsb;
+      
+      break;
+
+    case ITA2_NONE: // this char cannot be represented in ITA2
+      break;
+    }    
+  }
+
+  return(input_len);
+}
